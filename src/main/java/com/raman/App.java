a0cdc6f6cdecdd372b8a251391162295cd22c0b9
Arya Chandan Reddy <75093830+Astound@users.noreply.github.com>
2024-01-16 14:54:13 +0530

    Update App.java

--- src/main/java/com/raman/App.java
+++ src/main/java/com/raman/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Edo ithundi ra mawa" );
+        System.out.println( "Edo ithundi ra mawa, marchina malla" );
     }
 }
